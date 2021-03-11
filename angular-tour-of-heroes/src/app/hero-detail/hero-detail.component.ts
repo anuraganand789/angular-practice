a5e6ee2ca6cb8a7cb89153775a52993b6e5345f7
Anurag Anand <anuraganand789@hotmail.com>
2021-03-12 02:58:14 +0530

    fixed isseus and this is a completely working solution

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit , Input } from '@angular/core';
 import { Hero }              from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location }      from '@angular/common';
+import { HeroService }   from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -9,13 +13,35 @@ import { Hero }              from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero? : Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location : Location,
+    private heroService : HeroService
+  ) {}
 
   ngOnInit(): void {
+    this.setHero();
+  }
+
+  setHero() : void{
+    //+ converts string to number
+    const heroId = +this.route.snapshot.paramMap.get('id')!;
+    this
+      .heroService
+      .getHero(heroId)
+      .subscribe((hero: Hero) => this.hero = hero);
   }
 
   clear() : void {
     this.hero = undefined;
+  }
+
+  goBack() : void {
+    this.location.back();
+  }
+
+  save() : void {
+    this.heroService.updateHero((this.hero as Hero)).subscribe(() => this.goBack());
   }
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,24 +10,30 @@ import { MessageService }    from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero? : Hero;
   heroes        : Hero[] = [];
 
-  constructor(private heroService    : HeroService,
-              private messageService : MessageService) { }
+  constructor(private heroService    : HeroService){}
 
-  getHeroes() : void {
-    this.heroService.getHeroes()
+  loadHeroes() : void {
+    this.heroService
+        .getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
-    this.getHeroes();
+    this.loadHeroes();
   }
 
-  onSelect(hero: Hero) : void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent : Selected hero id =${hero.id}`);
+   add(name: string) : void {
+    if(!(name = name.trim())) { return ; }
+    this.heroService
+        .addHero({ name } as Hero)
+	.subscribe( hero => { this.heroes.push(hero) });
+  }
+
+   delete(heroToBeDeleted : Hero) : void{
+    this.heroes = this.heroes.filter(hero => heroToBeDeleted !== hero);
+    this.heroService.deleteHero(heroToBeDeleted).subscribe();
   }
 
 }
